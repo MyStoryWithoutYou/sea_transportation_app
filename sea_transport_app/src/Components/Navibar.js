0642cedd41657ad0cb59cdace7f7fa7007ae2f06
MyStoryWithoutYou <allanhaff@gmail.com>
2021-03-18 03:17:12 +0300

    created header using react bootstrap

---
+++ sea_transport_app/src/Components/Navibar.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav, Button } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+
+export default function Navibar() {
+    return (
+    <>
+        <Navbar collapseOnSelect expands="lg" bg="dark" variant="dark">
+            <Navbar.Brand>Sea Transportation Company</Navbar.Brand>
+            <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+            <Navbar.Collapse id="responsive-navbar-nav">
+                <Nav className="mr-auto">
+                    <Nav.Link><Link to="/">Home</Link></Nav.Link>
+                    <Nav.Link><Link to="/about">About</Link></Nav.Link>
+                </Nav>
+                <Nav>
+                    <Button variant="primary">Profile</Button>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
+    </>
+    )
+}
