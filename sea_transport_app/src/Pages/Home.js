dad2963143326cac688eabd6771c46f0f31ad35d
MyStoryWithoutYou <allanhaff@gmail.com>
2021-03-25 03:15:55 +0300

    added pagination bar

--- sea_transport_app/src/Pages/Home.js
+++ sea_transport_app/src/Pages/Home.js
@@ -2,12 +2,14 @@ import React from "react";
 
 import SearchBar from '../Components/SearchBar';
 import OrderTable from '../Components/OrderTable';
+import PaginationBar from '../Components/PaginationBar';
 
 const Home = () => {
     return(
         <div align="center">
             <SearchBar />
             <OrderTable />
+            <PaginationBar />
         </div>
     )
 }
